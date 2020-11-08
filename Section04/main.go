9b8860d3ad3c34f9c3f0d914aa169a07a7907017
JiaHongLin <joe830507@gmail.com>
2020-11-08 21:28:00 +0800

    teaching content
    declare a variable
    short declared variable
    create my own type
    conversion myType to int

--- Section04/main.go
+++ Section04/main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	assignValueToVariable()
+	declaredMyType()
+}
+
+var x string
+
+func assignValueToVariable() {
+	x = "Hello World"
+	y := "Hello World"
+	fmt.Println(x)
+	fmt.Println(y)
+}
+
+type myType int
+
+var z myType
+
+func declaredMyType() {
+	fmt.Printf("Value:%v,Type:%T\n", z, z)
+	z = 100
+	fmt.Printf("Value:%v,Type:%T\n", z, z)
+	s := int(z)
+	fmt.Printf("Value:%v,Type:%T\n", s, s)
 }
